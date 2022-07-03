d2de032246bbeed09af8c6241112daf67106fc30
daphneboelhouwers <daphneboelhouwers@gmail.com>
2022-07-03 17:49:55 +0200

    [Add] StartsContainsEnds DFA
    Needs testing!

--- src/main/model/automata/FA.java
+++ src/main/model/automata/FA.java
@@ -1,5 +1,7 @@
 package main.model.automata;
 
+import main.logic.Ndfa2DfaConverter;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -74,4 +76,84 @@ public class FA {
         for (Transition t : transitions) System.out.println(t.toString());
     }
 
+    public void startsWith(String input){
+        NDFA ndfa = new NDFA();
+
+        for(int i = 0; i < input.length(); i++){
+
+            String currentState = "q" + (i+1);
+            String nextState = "q" + (i+2);
+            String symbol = input ;
+
+            ndfa.addTransition(new Transition(currentState,nextState,symbol));
+        }
+
+        String endState = "q" + (input.length());
+        ndfa.addTransition(new Transition(endState, endState, "a"));
+        ndfa.addTransition(new Transition(endState, endState, "b"));
+
+        ndfa.addStartState("q1");
+        ndfa.addEndState(endState);
+        ndfa.getStartStates().add("q1");
+        ndfa.getEndStates().add(endState);
+
+        Ndfa2DfaConverter ndfa2DfaConverter = new Ndfa2DfaConverter();
+        ndfa2DfaConverter.convert(ndfa);
+    }
+
+    public void endsWith(String input){
+        NDFA ndfa = new NDFA();
+
+        for(int i = 0; i < input.length(); i++){
+
+            String currentState = "q" + (i+1);
+            String nextState = "q" + (i+2);
+            String symbol = input ;
+
+            ndfa.addTransition(new Transition(currentState,nextState,symbol));
+
+            String returnSymbol = symbol == "a" ? "b" : "a";
+            ndfa.addTransition(new Transition(currentState, "q1", returnSymbol));
+        }
+
+        String endState = "q" + (input.length()+ 1);
+        ndfa.addTransition(new Transition(endState, "q1", "a"));
+        ndfa.addTransition(new Transition(endState, "q1", "b"));
+
+        ndfa.addStartState("q1");
+        ndfa.addEndState(endState);
+        ndfa.getStartStates().add("q1");
+        ndfa.getEndStates().add(endState);
+
+        Ndfa2DfaConverter ndfa2DfaConverter = new Ndfa2DfaConverter();
+        ndfa2DfaConverter.convert(ndfa);
+    }
+
+    public void Contains(String input){
+        NDFA ndfa = new NDFA();
+
+        for(int i = 0; i < input.length(); i++){
+
+            String currentState = "q" + (i+1);
+            String nextState = "q" + (i+2);
+            String symbol = input ;
+
+            ndfa.addTransition(new Transition(currentState,nextState,symbol));
+
+            String returnSymbol = symbol == "a" ? "b" : "a";
+            ndfa.addTransition(new Transition(currentState, "q1", returnSymbol));
+        }
+
+        String endState = "q" + (input.length() + 1);
+        ndfa.addTransition(new Transition(endState, endState, "a"));
+        ndfa.addTransition(new Transition(endState, endState, "b"));
+
+        ndfa.addStartState("q1");
+        ndfa.addEndState(endState);
+        ndfa.getStartStates().add("q1");
+        ndfa.getEndStates().add(endState);
+
+        Ndfa2DfaConverter ndfa2DfaConverter = new Ndfa2DfaConverter();
+        ndfa2DfaConverter.convert(ndfa);
+    }
 }
